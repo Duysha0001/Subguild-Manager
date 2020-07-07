d0309096364703b9266910a81e31ecfb468b23d0
EQUENOS <50338932+EQUENOS@users.noreply.github.com>
2020-07-07 14:14:47 +0300

    Add files via upload

--- cogs/events.py
+++ cogs/events.py
@@ -12,6 +12,7 @@ db = cluster["guild_data"]
 
 #---------- Variables ------------
 xo_award = 1
+xol_award = 3
 
 #---------- Functions ------------
 from functions import has_permissions, detect, get_field, Leaderboard, read_message, trigger_reaction, is_command
@@ -39,6 +40,21 @@ def anf(user):
             out += s
     return out
 
+def isnested(section, array):
+    i = 0; v = 0; res = False
+    slen = len(section); alen = len(array)
+    while i < alen:
+        if v < slen:
+            if array[i] == section[v]:
+                v += 1
+            else:
+                v = 0
+        if v >= slen:
+            res = True
+            break
+        i += 1
+    return res
+
 def get_subguild(collection_part, subguild_sign):
     out = None
     if collection_part != None and "subguilds" in collection_part:
@@ -62,16 +78,21 @@ async def post_log(guild, channel_id, log):
         if channel is not None:
             await channel.send(embed=log)
 
-class XO_field:
-    def __init__(self, rows=3, columns=3):
+
+class XO_universal:
+    def __init__(self, rows=3, columns=3, req=3):
         self.matrix = [[0 for j in range(columns)] for i in range(rows)]
         self.moves = 0
         self.max_moves = rows * columns
+        self.rows = rows; self.columns = columns
+        self.req = req
     
     def display(self):
-        shapes = [":white_large_square:", ":negative_squared_cross_mark:", ":o2:"]
-        row_num_textures = [":one:", ":two:", ":three:"]
-        output = ":arrow_lower_right::regional_indicator_a::regional_indicator_b::regional_indicator_c:\n"
+        shapes = ["⬜", "❎", ":o2:"]
+        row_num_textures = [":one:", ":two:", ":three:", ":four:", ":five:", ":six:", ":seven:", ":eight:", ":nine:", ":keycap_ten:"]
+        col_letter_textures = [f":regional_indicator_{l}:" for l in ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]]
+        left_upper_corner = ":arrow_lower_right:"
+        output = f"{left_upper_corner}{''.join(col_letter_textures[:self.columns])}\n"
         for row, line in enumerate(self.matrix):
             display_line = str(row_num_textures[row])
             for value in line:
@@ -80,23 +101,30 @@ class XO_field:
         return output[:-1]
     
     def get_looser(self):
-        d = self.moves % 2 
-        return 1 if d > 0 else 2
+        if self.moves < 2:
+            return 0
+        else:
+            d = self.moves % 2
+            return 1 if d > 0 else 2
 
     def to_tuple(self, chess_formated):
-        digits = [str(i) for i in range(10)]
-        row = None
-        if chess_formated[0] in digits:
-            row = int(chess_formated[0]) - 1
-            col = chess_formated[1]
-        elif chess_formated[1] in digits:
-            row = int(chess_formated[1]) - 1
-            col = chess_formated[0]
-        
-        if row is not None:
-            if col in ["a", "b", "c"] and abs(row - 1) <= 1:
-                col = ["a", "b", "c"].index(col)
-                return (row, col)
+        # 1 letter is required. Not more and mot less
+        chess_formated = chess_formated.lower()
+        letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+        # Check
+        def check(l, d):
+            if l in letters and d.isdigit():
+                col = letters.index(l)
+                row = int(d) - 1
+                if (0 <= col < self.columns) and (0 <= row < self.rows):
+                    return (row, col)
+        # Check Letter|Digits
+        res = check(chess_formated[0], chess_formated[1:])
+        if res is None:
+            # Check Digits|Letter
+            return check(chess_formated[-1:], chess_formated[:-1])
+        else:
+            return res
     
     def put(self, coords, value):
         if len(coords) >= 2:
@@ -107,29 +135,89 @@ class XO_field:
                 self.moves += 1
     
     def find_winners(self):
-        lcross, rcross = [], []
-        col_count_1, col_count_2 = [0, 0, 0], [0, 0, 0]
-        for num, row in enumerate(self.matrix):
-            if row.count(1) == 3:
+        res = 0
+        lds = []; rds = []; cols = []
+        ldcs = []; rdcs = []
+        xrow = self.req * [1]; orow = self.req * [2]
+
+        def check(sec):
+            if isnested(xrow, sec):
                 return 1
-            elif row.count(2) == 3:
+            elif isnested(orow, sec):
                 return 2
-            for col, v in enumerate(row):
-                if v == 1:
-                    col_count_1[col] += 1
-                elif v == 2:
-                    col_count_2[col] += 1
-            lcross.append(row[num])
-            rcross.append(row[2 - num])
-        
-        if lcross.count(1) == 3 or rcross.count(1) == 3 or 3 in col_count_1:
-            return 1
-        elif lcross.count(2) == 3 or rcross.count(2) == 3 or 3 in col_count_2:
-            return 2
-        elif self.moves >= self.max_moves:
-            return 0
+            else:
+                return 0
+
+        for r, row in enumerate(self.matrix):
+            # Row check
+            res = check(row)
+            if res > 0:
+                break
+
+            for c, cell in enumerate(row):
+                # Filling columns
+                if c >= len(cols) and self.rows >= self.req:
+                    cols.append([cell])
+                else:
+                    cols[c].append(cell)
+                
+                # Filling left diagonals
+                try:
+                    n = ldcs.index((r, c))
+                except Exception:
+                    if self.columns - c >= self.req and self.rows - r >= self.req:
+                        ldcs.append((r + 1, c + 1))
+                        lds.append([cell])
+                else:
+                    ldcs[n] = (r + 1, c + 1)
+                    lds[n].append(cell)
+                
+                # Filling right diagonals
+                try:
+                    n = rdcs.index((r, c))
+                except Exception:
+                    if c + 1 >= self.req and self.rows - r >= self.req:
+                        rdcs.append((r + 1, c - 1))
+                        rds.append([cell])
+                else:
+                    rdcs[n] = (r + 1, c - 1)
+                    rds[n].append(cell)
+            
+        # Columns check
+        if res > 0:
+            return res
         else:
-            return None
+            del ldcs, rdcs
+            for s in cols:
+                res = check(s)
+                if res > 0:
+                    break
+            del cols
+            # Left diagonals check
+            if res > 0:
+                return res
+            else:
+                for s in lds:
+                    res = check(s)
+                    if res > 0:
+                        break
+                del lds
+                # Right diagonals check
+                if res > 0:
+                    return res
+                else:
+                    for s in rds:
+                        res = check(s)
+                        if res > 0:
+                            break
+                    del rds
+                    # Final return
+                    if res > 0:
+                        return res
+                    elif self.moves >= self.max_moves:
+                        return 0
+                    else:
+                        return None
 
 
 class events(commands.Cog):
@@ -312,7 +400,7 @@ class events(commands.Cog):
                     await ctx.send(embed=game_info)
 
                     players = (ctx.author, opponent)
-                    xo = XO_field()
+                    xo = XO_universal()
                     screen = await ctx.send(xo.display())
                     winner = None
 
@@ -338,7 +426,7 @@ class events(commands.Cog):
                     if winner is not None:
                         if winner == 0:
                             reply = discord.Embed(
-                                title="⚖ Ничья",
+                                title="⚖ | 3x3 | Ничья",
                                 color=discord.Color.orange()
                             )
                             reply.add_field(name="**Игрок 1**", value=str(players[0]))
@@ -367,11 +455,187 @@ class events(commands.Cog):
                             )
 
                             reply = discord.Embed(
-                                title=f"🏆 Выиграл {winner}",
+                                title=f"🏆 |3x3| Выиграл {winner}",
                                 color=discord.Color.gold()
                             )
                             reply.add_field(name="**Игрок 1**", value=f"+{xo_award} 🔅 | {anf(winner)}\nГильдия: {guild_names[winner.id]}")
                             reply.add_field(name="**Игрок 2**", value=f"-{xo_award} 🔅 | {anf(looser)}\nГильдия: {guild_names[looser.id]}")
+                            await ctx.send(embed=reply)
+
+                            await post_log(ctx.guild, log_channel, reply)
+
+    @commands.cooldown(1, 3, commands.BucketType.member)
+    @commands.command(aliases=["xo-large", "xol", "xo-big"])
+    async def xo_large(self, ctx, *, string):
+        p = ctx.prefix
+        opponent = detect.member(ctx.guild, string)
+        if opponent is None:
+            reply = discord.Embed(
+                title="💢 Упс",
+                description=f"Не могу найти пользователя по запросу **{string}**",
+                color=mmorpg_col("vinous")
+            )
+            reply.set_footer(text = f"{ctx.author}", icon_url = f"{ctx.author.avatar_url}")
+            await ctx.send(embed = reply)
+
+        elif opponent.id == ctx.author.id:
+            reply = discord.Embed(
+                title="❓ С собой играть нельзя",
+                description="Найдите себе соперника, с собой Вы не поиграете, увы.",
+                color=mmorpg_col("vinous")
+            )
+            reply.set_footer(text = f"{ctx.author}", icon_url = f"{ctx.author.avatar_url}")
+            await ctx.send(embed = reply)
+        
+        else:
+            collection = db["subguilds"]
+            result = collection.find_one(
+                {"_id": ctx.guild.id},
+                projection={
+                    f"subguilds.members.{opponent.id}": True,
+                    f"subguilds.members.{ctx.author.id}": True,
+                    "subguilds.name": True,
+                    "games_allowed": True,
+                    "log_channel": True
+                }
+            )
+            auth_guild = get_subguild(result, ctx.author.id)
+            oppo_guild = get_subguild(result, opponent.id)
+            games_allowed = get_field(result, "games_allowed", default=False)
+            log_channel = get_field(result, "log_channel")
+            guild_names = {
+                ctx.author.id: get_field(auth_guild, "name"),
+                opponent.id: get_field(oppo_guild, "name")
+            }
+            del result
+
+            if not games_allowed:
+                reply = discord.Embed(
+                    title="❌ На этом сервере ивент выключен",
+                    description=f"Включить: `{p}enable-event`",
+                    color=mmorpg_col("vinous")
+                )
+                reply.set_footer(text = f"{ctx.author}", icon_url = f"{ctx.author.avatar_url}")
+                await ctx.send(embed = reply)
+
+            elif auth_guild is None or oppo_guild is None:
+                if auth_guild is None:
+                    title = "Вы не в гильдии"
+                    desc = f"Для вступления в гильдию напишите `{p}join Название гильдии`"
+                else:
+                    title = f"{anf(opponent)} не в гильдии"
+                    desc = "Поищите другого оппонента"
+                
+                reply = discord.Embed(
+                    title=title,
+                    description=desc,
+                    color=mmorpg_col("vinous")
+                )
+                reply.set_footer(text = f"{ctx.author}", icon_url = f"{ctx.author.avatar_url}")
+                await ctx.send(embed = reply)
+            
+            elif auth_guild == oppo_guild:
+                reply = discord.Embed(
+                    title="❌ Ошибка",
+                    description="Противник должен быть из другой гильдии",
+                    color=mmorpg_col("vinous")
+                )
+                reply.set_footer(text = f"{ctx.author}", icon_url = f"{ctx.author.avatar_url}")
+                await ctx.send(embed = reply)
+
+            else:
+                request_emb = discord.Embed(
+                    title=f"{anf(ctx.author)} бросил Вам вызов | 10x10",
+                    description="✅ - принять\n\n❌ - отказаться",
+                    color=ctx.author.color
+                )
+                request = await ctx.send(f"{opponent.mention}", embed=request_emb)
+                await request.add_reaction("✅")
+                await request.add_reaction("❌")
+
+                payload = await trigger_reaction(request, ["✅", "❌"], opponent, 60, self.client)
+                if payload is None:
+                    request_emb = discord.Embed(description=f"Вызов от {anf(ctx.author)} был проигнорирован участником **{anf(opponent)}** (прошло 60 секунд)")
+                    await request.edit(embed=request_emb)
+                elif payload[0].emoji == "❌":
+                    request_emb = discord.Embed(
+                        description=f"**{anf(opponent)}** отказался от вызова **{anf(ctx.author)}**",
+                        color=discord.Color.dark_red()
+                    )
+                    await request.edit(embed=request_emb)
+                else:
+                    await request.delete()
+
+                    players = (ctx.author, opponent)
+                    xo = XO_universal(10, 10, 5)
+                    winner = None
+
+                    game_info = discord.Embed(
+                        title="🔰 Раунд",
+                        description=xo.display(),
+                        color=discord.Color.blue()
+                    )
+                    game_info.add_field(name="**(:negative_squared_cross_mark:) Первый ходит:**", value=str(ctx.author), inline=False)
+                    game_info.add_field(name="**(:o2:) Второй ходит:**", value=str(opponent), inline=False)
+                    screen = await ctx.send(embed=game_info)
+
+                    while winner is None:
+                        player = players[xo.moves % 2]
+                        msg = await read_message(ctx.channel, player, 60, self.client)
+                        if msg is None:
+                            winner = xo.get_looser()
+                        elif "quit" in msg.content.lower():
+                            winner = xo.get_looser()
+                        elif is_command(msg.content, ctx.prefix, self.client):
+                            winner = xo.get_looser()
+                        else:
+                            xo.put(msg.content.lower(), xo.moves % 2 + 1)
+                            winner = xo.find_winners()
+                            game_info.description = xo.display()
+                            await screen.edit(embed=game_info)
+
+                            try:
+                                await msg.delete()
+                            except Exception:
+                                pass
+                    
+                    if winner is not None:
+                        if winner == 0:
+                            reply = discord.Embed(
+                                title="⚖ | 10х10 | Ничья",
+                                color=discord.Color.orange()
+                            )
+                            reply.add_field(name="**Игрок 1**", value=str(players[0]))
+                            reply.add_field(name="**Игрок 2**", value=str(players[1]))
+                            await ctx.send(embed=reply)
+
+                        else:
+                            looser = players[winner % 2]
+                            winner = players[winner - 1]
+
+                            collection.update_one(
+                                {
+                                    "_id": ctx.guild.id,
+                                    "subguilds.name": guild_names[looser.id],
+                                    f"subguilds.members.{looser.id}": {"$exists": True}
+                                },
+                                {"$inc": {f"subguilds.$.reputation": -xol_award}}
+                            )
+                            collection.update_one(
+                                {
+                                    "_id": ctx.guild.id,
+                                    "subguilds.name": guild_names[winner.id],
+                                    f"subguilds.members.{winner.id}": {"$exists": True}
+                                },
+                                {"$inc": {f"subguilds.$.reputation": xol_award}}
+                            )
+
+                            reply = discord.Embed(
+                                title=f"🏆 | 10x10 | Выиграл {winner}",
+                                color=discord.Color.gold()
+                            )
+                            reply.add_field(name="**Игрок 1**", value=f"+{xol_award} 🔅 | {anf(winner)}\nГильдия: {guild_names[winner.id]}")
+                            reply.add_field(name="**Игрок 2**", value=f"-{xol_award} 🔅 | {anf(looser)}\nГильдия: {guild_names[looser.id]}")
                             await ctx.send(embed=reply)
 
                             await post_log(ctx.guild, log_channel, reply)
